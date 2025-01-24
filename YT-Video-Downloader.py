abf39b0802c24f0ea52bc28b0b765e1d05ec6c0b
Suraj Sahu <122232215+surajsahumrj@users.noreply.github.com>
2025-01-25 04:36:46 +0530

    Update YT-Video-Downloader.py

--- YT-Video-Downloader.py
+++ YT-Video-Downloader.py
@@ -1,3 +1,13 @@
+# Install pytube if not installed
+# pip install pytube
+
 from pytube import YouTube
 
-YouTube('https://youtu.be/peBsfgbOlYM?si=XUvjCHK8XwLp0tGE').streams.first().download()
+url = 'https://youtu.be/peBsfgbOlYM?si=XUvjCHK8XwLp0tGE'
+
+yt = YouTube(url)
+
+stream = yt.streams.get_highest_resolution()
+stream.download()
+
+print("Download completed!")
